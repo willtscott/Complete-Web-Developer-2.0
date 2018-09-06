26ea9590a31c36a4e6065e956e3afea119605da4
Will Scott <wtscott@gmail.com>
2018-09-06 11:49:34 +0200

    Adding repository to Github.com

---
+++ Chapter 1/index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Will Scott</title>
+    </head>
+        
+    <body style="background-color: bisque">
+        <p id="top">
+            <h1>Will Scott</h1>
+            <img src="photo.png" style="inline" width="200" align="right">
+        </p>
+        <table style="width: 100%">
+            <thead>
+                <tr>
+                    <th>Coder</th>
+                    <th>Traveler</th>
+                    <th>Environmentalist</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>C++</li>
+                            <li>Python</li>
+                            <li>Javascript</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Asia</li>
+                            <li>Oceania</li>
+                            <li>Mediterrania</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Volunteer</li>
+                            <li>Recycler</li>
+                            <li>Hiker</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <hr>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        Hello, this is my personal webpage to showcase my projects and accomplishments. I built this website, too.
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Here's a list of links to sites where you can find out more about me. 
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <ol>
+                <table style="width: 100%">
+                    <tr>
+                        
+                        <td>
+                            <li>
+                                <a href="https://www.linkedin.com/in/willtscott/">LinkedIn</a>
+                            </li>
+                        </td>
+                        <td>
+                            <li>
+                                <a href="http://www.instagram.com/uull_/">Instagram</a>
+                            </li>
+                        </td>
+                        <td>
+                            <li>
+                                <a href="https://shopfolk.org/">&lt;&gt;FOLK&lt;&gt;</a>
+                            </li>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <iframe src="https://www.linkedin.com/in/willtscott/" width="200"></iframe>
+                        </td>
+                        <td>
+                            <iframe src="http://www.instagram.com/uull_/" width="200"></iframe>
+                        </td>
+                        <td>
+                            <iframe src="http://shopfolk.org" width="200"></iframe>     
+                        </td>
+                    </tr>
+            </table>
+        </ol>
+
+        <form action="file:///Users/willscott/Documents/GitHub/Complete%20Web%20Developer%202.0/Chapter%201/WillScott.html?#top">
+            <strong>Send me a message!</strong>
+            <br>
+            Your Email: 
+            <br>
+            <input type="text" placeholder="name@example.com"></input>
+            <br>
+            Message: 
+            <br>
+            <input type="text" placeholder="Your message...." size="100" maxlength="1000"></input>
+            <br>
+            <input type="submit" value="Send">   
+        </form>
+        <br>
+        <a href="#top">Top of the Page</a>
+    </body>
+</html>
